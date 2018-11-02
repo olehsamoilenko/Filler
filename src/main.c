349e3ed8b805ee91688275a22417c1b36aafa6f9
Oleh SAMOILENKO <osamoile@e1r6p9.unit.ua>
2018-11-02 18:26:16 +0200

    reshaping

--- src/main.c
+++ src/main.c
@@ -13,7 +13,7 @@
 #include <filler.h>
 #include <fcntl.h> // trahs
 
-void		put_piece(t_array map, int ***dist_map, t_array piece, char me, char opponent)
+void		put_piece(t_array map, int ***dist_map, t_array piece, char me, char opponent, int fd, int start_x, int start_y)
 {
 	int i;
 	int j;
@@ -61,10 +61,29 @@ void		put_piece(t_array map, int ***dist_map, t_array piece, char me, char oppon
 
 		}
 	}
-	ft_printf("%i %i\n", put_x, put_y);
+	// ft_putnbr_fd(put_x, fd);
+	// ft_putstr_fd(" - figure x\n", fd);
+	// ft_putnbr_fd(put_y, fd);
+	// ft_putstr_fd(" - figure y\n", fd);
+	ft_printf("%i %i\n", put_x - start_x, put_y - start_y);
+
+	// ft_putstr_fd("FROM PUT_PIECE:\n", fd);
+	// int i = -1;
+	// while (++i < map.x)
+	// {
+	// 	int j = -1;
+	// 	while (++j < map.y)
+	// 	{
+	// 		ft_putnbr_fd((*dist_map)[i][j], fd);
+	// 		ft_putchar_fd(' ', fd);
+	// 	}
+	// 	ft_putchar_fd('\n', fd);
+	// }
+	// ft_putchar_fd('\n', fd);
+	// ft_printf("%i %i\n", 0, 0);
 }
 
-void		fill_map(t_array map)
+void		fill_map(t_array map, int fd)
 {
 	int		i;
 	char	*line;
@@ -73,9 +92,12 @@ void		fill_map(t_array map)
 	while (++i < map.x)
 	{
 		get_next_line(0, &line);
+		ft_putstr_fd(line, fd);
+		ft_putchar_fd('\n', fd);
 		ft_strcpy(&map.array[i][0], &line[4]);
 		ft_strdel(&line);
 	}
+	// ft_putchar_fd('\n', fd);
 }
 
 char	**ft_chartable(int x, int y)
@@ -93,41 +115,41 @@ char	**ft_chartable(int x, int y)
 	return (table);
 }
 
-void		create_map(char *plateau, t_array *map, int ***dist_map)
+t_array		create_map(char *plateau, int ***dist_map)
 {
 	char	**output;
 	int		i;
 	int		j;
+	t_array	map;
 
 		output = ft_strsplit(plateau, ' ');
-		map->x = ft_atoi(output[1]);
-		map->y = ft_atoi(output[2]);
+		map.x = ft_atoi(output[1]);
+		map.y = ft_atoi(output[2]);
 		ft_arrclr(output);
-		// map->array = ft_memalloc((map->x + 1) * sizeof(char*));
-		// map->array[map->x] = NULL;
-		map->array = ft_chartable(map->x, map->y);
-		*dist_map = (int**)ft_memalloc(map->x * sizeof(int*));
+		// map.array = ft_memalloc((map.x + 1) * sizeof(char*));
+		// map.array[map.x] = NULL;
+		map.array = ft_chartable(map.x, map.y);
+		*dist_map = (int**)ft_memalloc(map.x * sizeof(int*));
 		i = -1;
-		while (++i < map->x)
+		while (++i < map.x)
 		{
-			// map->array[i] = ft_strnew(map->y);
-			(*dist_map)[i] = (int*)ft_memalloc(map->y * sizeof(int));
+			// map.array[i] = ft_strnew(map.y);
+			(*dist_map)[i] = (int*)ft_memalloc(map.y * sizeof(int));
 			j = -1;
-			while (++j < map->y)
+			while (++j < map.y)
 				(*dist_map)[i][j] = -1;
 		}
+		return (map);
 }
 
 
-void			reshape(t_array *piece, int fd)
+void			reshape(t_array *piece, int fd, int *start_x, int *start_y)
 {
 	int		i;
 	int		j;
 	int		x_found = 0;
 	int		y_found = 0;
-	int		start_x;
 	int		end_x;
-	int		start_y;
 	int		end_y;
 
 	i = -1;
@@ -138,7 +160,7 @@ void			reshape(t_array *piece, int fd)
 			end_x = i;
 			if (!x_found)
 			{
-				start_x = i;
+				*start_x = i;
 				x_found = 1;
 			}
 		}
@@ -155,19 +177,20 @@ void			reshape(t_array *piece, int fd)
 				end_y = i;
 				if (!y_found)
 				{
-					start_y = i;
+					*start_y = i;
 					y_found = 1;
 				}
 			}
 				
 		}
 	}
-	if (end_x - start_x + 1 != piece->x || end_y - start_y + 1 != piece->y)
+	if (end_x - *start_x + 1 != piece->x || end_y - *start_y + 1 != piece->y)
 	{
-		ft_putstr_fd("NEED TO RESHAPE\n", fd);
+		// ft_putstr_fd("NEED TO RESHAPE\n", fd);
 		char **buf = piece->array;
-		piece->x = end_x - start_x + 1;
-		piece->y = end_y - start_y + 1;
+		
+		piece->x = end_x - *start_x + 1;
+		piece->y = end_y - *start_y + 1;
 		piece->array = ft_chartable(piece->x, piece->y);
 		i = -1;
 		while (++i < piece->x)
@@ -175,36 +198,40 @@ void			reshape(t_array *piece, int fd)
 			j = -1;
 			while (++j < piece->y)
 			{
-				piece->array[i][j] = buf[i + start_x][j + start_y];
+				piece->array[i][j] = buf[i + *start_x][j + *start_y];
 			}
 		}
+		ft_arrclr(buf);
 	}
+
 		
 }
 
 
-void		take_piece(char *params, t_array *piece)
+t_array		take_piece(char *params)
 {
 	char	*line;
 	char	**output;
 	int		i;
+	t_array	piece;
 
 		output = ft_strsplit(params, ' ');
 		ft_strdel(&params);
-		piece->x = ft_atoi(output[1]);
-		piece->y = ft_atoi(output[2]);
+		piece.x = ft_atoi(output[1]);
+		piece.y = ft_atoi(output[2]);
 		ft_arrclr(output);
 		// piece->array = ft_memalloc((piece->x + 1) * sizeof(char*));
 		// piece->array[piece->x] = NULL;
-		piece->array = ft_chartable(piece->x, piece->y);
+		piece.array = ft_chartable(piece.x, piece.y);
 		i = -1;
-		while (++i < piece->x)
+		while (++i < piece.x)
 		{
 			get_next_line(0, &line);
 			// piece->array[i] = ft_strnew(piece->y);
-			ft_strcpy(&piece->array[i][0], &line[0]);
+			ft_strcpy(piece.array[i], &line[0]);
 			ft_strdel(&line);
 		}
+		return (piece);
 		
 	
 }
@@ -216,7 +243,7 @@ int			ft_abs(int n)
 	return (n);
 }
 
-void		calculate_distances(t_array map, int ***dist_map, char me, char opponent)
+void		distance_to_opponent(t_array map, int ***dist_map, char me, char opponent)
 {
 	int		i;
 	int		j;
@@ -247,6 +274,28 @@ void		calculate_distances(t_array map, int ***dist_map, char me, char opponent)
 	
 }
 
+int		distance_to_cell(t_array map, int ***dist_map, char player, int x, int y)
+{
+	int		i;
+	int		j;
+	int		min;
+
+	min = 10000;
+	i = -1;
+	while (++i < map.x)
+	{
+		j = -1;
+		while (++j < map.y)
+		{
+			(*dist_map)[i][j] = ft_abs(x - i) + ft_abs(y - j);
+			if (ft_tolower(map.array[i][j]) == player && (*dist_map)[i][j] < min)
+				min = (*dist_map)[i][j];
+		}
+	}
+	
+	return (min);
+}
+
 int			main(void)
 {
 	char	*line;
@@ -255,9 +304,23 @@ int			main(void)
 	t_array	piece;
 	char	me;
 	char	opponent;
+	int start_x;
+	int start_y;
+	int i;
+	int j;
 
 
-	int fd = open("file.txt", O_RDWR);
+	t_dot	*dots;
+	int n_dots;
+	
+
+
+
+	piece.array = NULL;
+
+
+	int debug = open("file.txt", O_RDWR);
+	int karta = open("map.txt", O_RDWR);
 
 	get_next_line(0, &line); // header $$$ exec p1 : [./osamoile.filler]
 	if (ft_strstr(line, "p1"))
@@ -277,71 +340,189 @@ int			main(void)
 		if (!ft_strstr(line, "Plateau"))
 			return (0);
 		if (map.array == NULL)
-			create_map(line, &map, &dist_map);
+		{
+			map = create_map(line, &dist_map);
+			n_dots = 25;
+			dots = ft_memalloc(n_dots * sizeof(t_dot));
+			
+			i = -1;
+			while (++i < 5)
+			{
+				j = -1;
+				while (++j < 5)
+				{
+					dots[5 * i + j].x = i * 1 * map.x / 5;
+					dots[5 * i + j].y = j * 1 * map.y / 5;
+				}
+			}
+			// dots[0].x = 3 * map.x / 4 ;
+			// dots[0].y = 3 * map.y / 4 ;
+
+			// dots[1].x = 1 * map.x / 2 ;
+			// dots[1].y = 3 * map.y / 4 ;
+
+			// dots[2].x = 3 * map.x / 4 ;
+			// dots[2].y = 1 * map.y / 2 ;
+
+			// dots[3].x = 1 * map.x / 2 ;
+			// dots[3].y = 1 * map.y / 2 ;
+
+			// dots[4].x = 1 * map.x / 4 ;
+			// dots[4].y = 3 * map.y / 4 ;
+
+			// dots[5].x = 3 * map.x / 4 ;
+			// dots[5].y = 1 * map.y / 4 ;
+
+			// dots[6].x = 1 * map.x / 4 ;
+			// dots[6].y = 1 * map.y / 2 ;
+
+			// dots[7].x = 1 * map.x / 2 ;
+			// dots[7].y = 1 * map.y / 4 ;
+
+			// dots[8].x = 1 * map.x / 4 ;
+			// dots[8].y = 1 * map.y / 4 ;
+
+		}
+			
 
 		ft_strdel(&line);
 		get_next_line(0, &line); // skip 0123456789
 		ft_strdel(&line);
-		fill_map(map);
+		fill_map(map, debug);
 
-		calculate_distances(map, &dist_map, me, opponent);
+		
 
 		// check
-		int i = -1;
-		while (++i < map.x)
-		{
-			int j = -1;
-			while (++j < map.y)
-			{
-				ft_putnbr_fd(dist_map[i][j], fd);
-				ft_putchar_fd(' ', fd);
-			}
-			ft_putchar_fd('\n', fd);
-		}
+		// int i = -1;
+		// while (++i < map.x)
+		// {
+		// 	int j = -1;
+		// 	while (++j < map.y)
+		// 	{
+		// 		ft_putnbr_fd(dist_map[i][j], debug);
+		// 		ft_putchar_fd(' ', debug);
+		// 	}
+		// 	ft_putchar_fd('\n', debug);
+		// }
 		// check
 
 
 		get_next_line(0, &line);
 		if (piece.array != NULL)
 			ft_arrclr(piece.array);
-		take_piece(line, &piece);
+		piece = take_piece(line);
 
 		// check
+		// i = -1;
+		// while (++i < piece.x)
+		// {
+		// 	ft_putstr_fd(piece.array[i], debug);
+		// 	ft_putchar_fd('\n', debug);
+		// }
+		// check
+
+		start_x = 0;
+		start_y = 0;
+		reshape(&piece, debug, &start_x, &start_y);
+
+		// check start
+		// ft_putnbr_fd(start_x, fd);
+		// ft_putstr_fd(" - start x\n", fd);
+		// ft_putnbr_fd(start_y, fd);
+		// ft_putstr_fd(" - start y\n", fd);
+		// check start
+
+		// check
+		// ft_putstr_fd("AFTER RESHAPING\n", debug);
 		i = -1;
 		while (++i < piece.x)
 		{
-			ft_putstr_fd(piece.array[i], fd);
-			ft_putchar_fd('\n', fd);
+			ft_putstr_fd(piece.array[i], debug);
+			ft_putchar_fd('\n', debug);
 		}
 		// check
 
-		// reshape(&piece, fd);
+		// int k = -1;
+		// int min;
+		// while (++k < n_dots)
+		// {
+		// 	min = distance_to_cell(map, &dist_map, me, dots[k].x, dots[k].y);
+		// 	ft_putnbr_fd(min, debug);
+		// 	ft_putstr_fd(" - min, ", debug);
+		// 	ft_putnbr_fd(k, debug);
+		// 	ft_putstr_fd(" - k\n", debug);
+		// 	if (min > 5)
+		// 		break;
+		// }
+		// if (k == n_dots)
+		// {
+		// 	ft_putstr_fd("attack!\n", debug);
+		// 	distance_to_opponent(map, &dist_map, me, opponent);
+		// }
 
-		// check
 		i = -1;
-		while (++i < piece.x)
+		int max_dist = 0;
+		int best = -1;
+		while (++i < n_dots)
 		{
-			ft_putstr_fd(piece.array[i], fd);
-			ft_putchar_fd('\n', fd);
-		}
-		// check
-
-		put_piece(map, &dist_map, piece, me, opponent);
-
-		// check
-		i = -1;
-		while (++i < map.x)
-		{
-			int j = -1;
-			while (++j < map.y)
+			dots[i].my_dist = distance_to_cell(map, &dist_map, me, dots[i].x, dots[i].y);
+			dots[i].op_dist = distance_to_cell(map, &dist_map, opponent, dots[i].x, dots[i].y);
+			if (dots[i].my_dist <= 1 || dots[i].op_dist <= 1)
+				dots[i].reached = 1;
+			ft_putnbr_fd(dots[i].reached, debug);
+			ft_putstr_fd(" Dot ", debug);
+			ft_putnbr_fd(i, debug);
+			ft_putstr_fd(" (", debug);
+			ft_putnbr_fd(dots[i].x, debug);
+			ft_putstr_fd(", ", debug);
+			ft_putnbr_fd(dots[i].y, debug);
+			ft_putstr_fd(") ", debug);
+			ft_putnbr_fd(dots[i].my_dist, debug);
+			ft_putstr_fd(" - my, ", debug);
+			ft_putnbr_fd(dots[i].op_dist, debug);
+			ft_putstr_fd(" - op, ", debug);
+			ft_putnbr_fd(dots[i].op_dist - dots[i].my_dist, debug);
+			ft_putstr_fd(" - diff\n", debug);
+			if (dots[i].reached == 0 && dots[i].op_dist > max_dist)
 			{
-				ft_putnbr_fd(dist_map[i][j], fd);
-				ft_putchar_fd(' ', fd);
+				max_dist = dots[i].op_dist;
+				best = i;
 			}
-			ft_putchar_fd('\n', fd);
+			
+			
 		}
-		ft_putchar_fd('\n', fd);
-		// check
+		ft_putnbr_fd(best, debug);
+		ft_putstr_fd(" - best, ", debug);
+		ft_putnbr_fd(max_dist, debug);
+		ft_putstr_fd(" - min diff\n", debug);
+		if (best != -1)
+		{
+			distance_to_cell(map, &dist_map, me, dots[best].x, dots[best].y);
+		}
+		else
+		{
+			distance_to_opponent(map, &dist_map, me, opponent);
+		}
+			
+
+		// i = -1;
+		// while (++i < map.x)
+		// {
+		// 	j = -1;
+		// 	while (++j < map.y)
+		// 	{
+		// 		ft_putnbr_fd(dist_map[i][j], debug);
+		// 		ft_putchar_fd(' ', debug);
+		// 	}
+		// 	ft_putchar_fd('\n', debug);
+		// }
+		// ft_putchar_fd('\n', debug);
+
+
+		put_piece(map, &dist_map, piece, me, opponent, debug, start_x, start_y);
+
+
+
 	}
 	return (0);
 }
